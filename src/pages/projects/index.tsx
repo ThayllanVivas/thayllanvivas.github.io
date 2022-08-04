4a3a476c217aa179e87fff00d98b6b5930549355
Thayllan Vivas <thayllanvivas@hotmail.com>
2022-08-04 00:06:27 -0300

    Project detail info are being added

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import styles from '../../styles/Projects.module.scss'
 import example from '../../assets/example.png'
+import themovielib from '../../assets/themovielib.png'
 
 export default function Projects(){
     return (
@@ -10,9 +11,12 @@ export default function Projects(){
                 <h2>PROJETOS</h2>
                 <div id={styles.project_container}>
                     <div className={styles.project}>
-                        <img src={example} />
-                        <h3>PROJETO 1</h3>
-                        <p>Descrição do projeto vem aqui</p>
+                        <img src={themovielib} />
+                        <h3>THE MOVIE LIB</h3>
+                        <p>
+                            Site que indica os top 20 filmes mais avaliados ao usuário. <br />
+                            Foi realizado com API do TMDB usando JavaScript e Vite;
+                        </p>
                     </div>
                     <div className={styles.project}>
                         <img src={example} />
